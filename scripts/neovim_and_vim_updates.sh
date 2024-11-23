53f515707b979d6eb535790da8c846c1516f8908
Rbtistab <russbatisbarrrco@gmail.com>
2024-11-23 09:32:06 -0600

    Updates on vim and neovim scritps + adding ideavimrc

---
+++ scripts/neovim_and_vim_updates.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+###############################################################################
+#                       Neovim and vim updates                                #
+###############################################################################
+
+###############################################################################
+# Updates from local neovim and vim to dotfiles package and viceversa
+###############################################################################
+
+PACKAGE_VIMRC_FILE="${DOTFILES_PACKAGE_DIR}/.vimrc"
+LOCAL_VIMRC_FILE="${HOME}/.vimrc"
+PACKAGE_IDEAVIMRC_FILE="${DOTFILES_PACKAGE_DIR}/.ideavimrc"
+LOCAL_IDEAVIMRC_FILE="${HOME}/.ideavimrc"
+PACKAGE_NVCHAD_DIR="${DOTFILES_PACKAGE_DIR}/config/nvim/nvchad-custom"
+LOCAL_NVCHAD_DIR="${HOME}/.config/nvim/lua/custom"
+
+nvchad_package_to_local() {
+  echo "Copying '.vimrc' file..."
+  cp -r ${PACKAGE_VIMRC_FILE} ${LOCAL_VIMRC_FILE} 
+
+  echo "Copying '.ideavimrc' file..."
+  cp -r ${PACKAGE_VIMRC_FILE} ${LOCAL_VIMRC_FILE} 
+
+  echo "Copying nvchad files from package to local..."
+  rsync -a --delete ${PACKAGE_NVCHAD_DIR}/ ${LOCAL_NVCHAD_DIR}
+  echo "Done!"
+}
+
+nvchad_local_to_package() {
+  echo "Copying '.vimrc' file..."
+  cp -r ${LOCAL_VIMRC_FILE} ${PACKAGE_VIMRC_FILE} 
+
+  echo "Copying '.ideavimrc' file..."
+  cp -r ${LOCAL_VIMRC_FILE} ${PACKAGE_VIMRC_FILE} 
+
+  echo "Copying nvchad files from local to package..."
+  rsync -a --delete ${LOCAL_NVCHAD_DIR}/ ${PACKAGE_NVCHAD_DIR}
+  echo "Done!"
+}
+
+
